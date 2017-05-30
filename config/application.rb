44c786108fc577b93877b695e947ba6dbab50be1
Samuel Witke <witkesam@gmail.com>
2017-05-30 14:18:17 -0400

    modified:   ../app/views_mobile/songs/search.html.erb 	modified:   application.rb

--- config/application.rb
+++ config/application.rb
@@ -18,9 +18,9 @@ module VoteSongQueue
 	config.before_initialize do
         config.client = SpotifyClient::Client.new
 		puts "Enter Client ID Created @ https://developer.spotify.com/"
-		client_id = gets
+		client_id = STDIN.gets
 		puts"Enter Client Secret"
-		client_secret= gets
+		client_secret= STDIN.gets
 		RSpotify.authenticate(client_id.chomp,client_secret.chomp)
    end
   end
