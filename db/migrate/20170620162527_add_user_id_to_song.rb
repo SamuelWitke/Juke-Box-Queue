26472e3be5e56735b8e2f9344afcffd2cba91570
Samuel Witke <witkesam@gmail.com>
2017-06-20 12:28:37 -0400

    modified:   app/views/songs/search.html.erb 	new file:   db/migrate/20170620162527_add_user_id_to_song.rb 	modified:   db/schema.rb 	modified:   public/assets/.sprockets-manifest-997d109a66084532c4a889fed243012f.json 	new file:   public/assets/application-2c421e75f73bb6cf1e576e6a0f100bf1103ea4ea3803c9ae38731103c3920b30.css 	new file:   public/assets/application-2c421e75f73bb6cf1e576e6a0f100bf1103ea4ea3803c9ae38731103c3920b30.css.gz

---
+++ db/migrate/20170620162527_add_user_id_to_song.rb
@@ -0,0 +1,4 @@
+class AddUserIdToSong < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414001403) do
+ActiveRecord::Schema.define(version: 20170620162527) do
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
